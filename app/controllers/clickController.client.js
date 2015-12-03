2da7b62d7b46be73a847ce4e37e93858efc1cfe0
Harvey Sanders <sanders.harvey@gmail.com>
2015-12-02 23:30:34 -0600

    complete AJAX function

--- app/controllers/clickController.client.js
+++ app/controllers/clickController.client.js
@@ -10,11 +10,23 @@
 		if (typeof fn !== 'function') {
 			return;
 		}
-		
+
 		if (document.readyState === 'complete') {
 			return fn();
 		}
+		document.addEventListener('DOMContentLoaded', fn, false); //read more for better understanding
 	}
 
-	document.addEventListener('DOMContentLoaded', fn, false);
+	function ajaxRequest (method, url, callback) {
+		var xmlhttp = new XMLHttpRequest();
+
+		xmlhttp.onReadyStateChange = function() {
+			if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+				callback(xmlhttp.response);
+			}
+		};
+		xmlhttp.open(method, url, true);
+		xmlhttp.send();
+	}
+	
 })();
