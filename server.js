e9329018f7e9b6a0d79c517553fc640ac3d013ae
Harvey Sanders <sanders.harvey@gmail.com>
2015-12-02 23:48:06 -0600

    fix controllers route

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
 		console.log('MongoDB successfully connected on port 27017');
 
 	app.use('/public', express.static(process.cwd() + '/public'));
-	app.use('/controllers', express.static(process.cwd() + '/controllers'));
+	app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
 	routes(app, db);
 
